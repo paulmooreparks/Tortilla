0840febd13bcc2393c546a9788da3efcfbfb1f7f
Paul Parks <paul@smartsam.com>
2018-08-23 17:53:57 +0800

    Pre-populate the IVT up to 0x7F

--- assembly/bios.asm
+++ assembly/bios.asm
@@ -5,13 +5,524 @@
 
 .section begin, "a"
 
-/* Playing around with pre-populating the interrupt vector table. */
-.word dummyint
-.word 0xf000
-.word dummyint
-.word 0xf000
-.word dummyint
-.word 0xf000
+/* Pre-populate the interrupt vector table. */
+
+int00:
+/* INT 0x00 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+int01:
+/* INT 0x01 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+int02:
+/* INT 0x02 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+int03:
+/* INT 0x03 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x04 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x05 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x06 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x07 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x08 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x09 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x0A */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x0B */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x0C */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x0D */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x0E */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x0F */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x10 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x11 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x12 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x13 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x14 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x15 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x16 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x17 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x18 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x19 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x1A */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x1B */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x1C */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x1D */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x1E */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x1F */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x20 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x21 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x22 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x23 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x24 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x25 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x26 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x27 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x28 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x29 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x2A */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x2B */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x2C */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x2D */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x2E */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x2F */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x00 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x31 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x32 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x33 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x34 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x35 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x36 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x37 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x38 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x39 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x3A */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x3B */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x3C */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x3D */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x3E */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x3F */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x40 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x41 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x42 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x43 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x44 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x45 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x46 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x47 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x48 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x49 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x4A */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x4B */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x4C */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x4D */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x4E */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x4F */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x50 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x51 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x52 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x53 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x54 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x55 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x56 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x57 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x58 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x59 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x5A */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x5B */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x5C */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x5D */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x5E */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x5F */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x60 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x61 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x62 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x63 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x64 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x65 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x66 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x67 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x68 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x69 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x6A */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x6B */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x6C */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x6D */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x6E */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x6F */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x70 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x71 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x72 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x73 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x74 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x75 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x76 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x77 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x78 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x79 */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x7A */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x7B */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x7C */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x7D */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x7E */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
+/* INT 0x7F */
+.word dummyint	/* interrupt handler entry point */
+.word 0xf000	/* code segment */
+
 
 .section main, "ax"
 
@@ -65,10 +576,6 @@ done:
 	mov es:[bx], word ptr 0x0E4B
 
 	hlt
-
-	/* Stuff to test later */
-	mov ah, 4
-	mov bl, 6
 
 dummyint:
 	/* Here goes the body of your handler */
