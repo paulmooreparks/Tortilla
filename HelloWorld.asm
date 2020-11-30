2036f5a5ac8318c1e8f3aaf0a5cc6b47d3a0eda7
Paul M. Parks <paul@smartsam.com>
2020-11-30 17:00:32 +0800

    Syncing local mods
    Syncing local mods

--- HelloWorld.asm
+++ HelloWorld.asm
@@ -6,23 +6,29 @@ LABEL hello_world_end   AUTO
 
 ; Startup routine
 _start:
-LD $0000,2000,0000,2000 S
+
+; Set stack pointer. The comma is used as a number separator. 
+; Underscore (_) and back-tick (`) may also be used as separators.
+LD $0000,2000 S.H0
+
+; Set base pointer
+LD S.H0 S.H1
 
 ; Write string
-CLR A
-LD $01 A
-LD $01 G
-LD hello_world H.H0 
-LD hello_world_end J
-SUB hello_world J
-INT $80
+CLR A                   ; Clear the A register (set to 0)
+LD $01 A                ; Load syscall opcode 1 (write) into A register
+LD $01 G                ; Load file descriptor 1 (STDOUT) into G register
+LD hello_world H.H0     ; Load the pointer to the string into H.H0 register
+LD hello_world_end J    ; Load the pointer to byte past end of string into J register
+SUB hello_world J       ; Subtract pointer to start of string from pointer to end of string,
+                        ; which gives the string length in J register
+INT $80                 ; Call interrupt $80 to execute write syscall
 
 ; "Power down" the system
-CLR A
 ; A.Q0 = $0000 -> shut down
+CLR A
 INT $40
 
 hello_world: 
 STRING "Hello, world!"
 hello_world_end:
-
